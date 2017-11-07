a5d7c6bbbf0a8c97721e50590897aadd58bd19ed
xusy <xusy@xingyuanauto.com>
2017-11-07 11:18:52 +0800

    js url

--- src/main/webapp/app/js/urlObj.js
+++ src/main/webapp/app/js/urlObj.js
@@ -1,6 +1,4 @@
-﻿//var domain="http://x.xingyuanauto.com/app/scheduled/";
-var domain="http://meet.oa.xingyuanauto.com/xyauto_meet/app/scheduled/";
-urlPrefix="../app/images/";
+﻿urlPrefix="../app/images/";
 var urlObj={
 		//会议室区域列表
 		scheduleOffice : domain + 'getInfoByOfficeId',
--- src/main/webapp/js/properties.js
+++ src/main/webapp/js/properties.js
@@ -2,8 +2,10 @@
 var OA_LOGIN = 'http://oa1.xingyuanauto.com/Login.aspx?gourl=http://meet.xingyuanauto.com/xyauto_meet/';
 var ROLE_PAGE = 'http://meet.xingyuanauto.com/xyauto_meet/role.html';
 var SYSTEM_ID = 'SYS023'
-
+var domain="http://meet.xingyuanauto.com/xyauto_meet/app/scheduled/";
+//正式服
 //var OA_URL = 'http://oa.xingyuanauto.com/';
 //var OA_LOGIN = 'http://oa.xingyuanauto.com/Login.aspx?gourl=http://meet.oa.xingyuanauto.com/xyauto_meet/';
 //var ROLE_PAGE = 'http://meet.oa.xingyuanauto.com/xyauto_meet/role.html';
 //var SYSTEM_ID = 'SYS024'
+//var domain="http://meet.oa.xingyuanauto.com/xyauto_meet/app/scheduled/";
