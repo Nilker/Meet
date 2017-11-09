d844ab253436cd1735cf861eccdf5189a97c6f97
qiaom <qiaom@xingyuanauto.com>
2017-11-09 15:35:19 +0800

    update

--- src/main/java/com/xyauto/controller/OAController.java
+++ src/main/java/com/xyauto/controller/OAController.java
@@ -23,33 +23,15 @@ import io.swagger.annotations.ApiOperation;
  */
 @RestController
 @RequestMapping("/oa")
-@Api(description="OA")
+@Api(description = "OA")
 public class OAController {
 
 	@Autowired
 	private OAService oas;
 
-//	@GetMapping("/department")
-//	@ApiOperation(value = "查询部门列表", notes = "查询所有部门列表")
-//	public ResultUtil queryDepartment() {
-//		return ResultUtil.success(oas.queryDepartment());
-//	}
-//
-//	@GetMapping("/employeeByDept/{id}")
-//	@ApiOperation(value = "查询员工名称", notes = "通过部门ID查询员工名称（不包含子部门）")
-//	public ResultUtil queryEmployeeByDept(@PathVariable Integer id) {
-//		return ResultUtil.success(oas.queryEmployeeByDept(id));
-//	}
-//	
-//	@GetMapping("/queryEmployeeByDeptIncludeChildren/{id}")
-//	@ApiOperation(value = "查询员工名称", notes = "通过部门ID查询员工名称（包含子部门）")
-//	public ResultUtil queryEmployeeByDeptIncludeChildren(@PathVariable Integer id) {
-//		return ResultUtil.success(oas.queryEmployeeByDeptIncludeChildren(id));
-//	}
-	
 	@GetMapping("/queryAllEmployee")
 	@ApiOperation(value = "查询员工名称", notes = "查询所有员工名称")
-	public ResultUtil queryAllEmployee(){
+	public ResultUtil queryAllEmployee() {
 		ResultUtil result = null;
 		try {
 			result = ResultUtil.success(oas.queryAllEmployee());
